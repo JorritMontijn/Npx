536501c7a87a75ee02892224321d7811f57aa908
JorritMontijn <jorritmontijn@gmail.com>
2023-02-22 13:15:56 +0100

    Update loadDataNpx.m

--- loadDataNpx.m
+++ loadDataNpx.m
@@ -76,6 +76,9 @@ function [sAggStim,sAggNeuron,sSources]=loadDataNpx(strArea,strRunStim,strDataSo
 					if isfield(sAP.sCluster,'dPrimeLR')
 						sAP.sCluster = rmfield(sAP.sCluster,'dPrimeLR');
 					end
+					if isfield(sAP.sCluster,'Waveform')
+						sAP.sCluster = rmfield(sAP.sCluster,'Waveform');
+					end
 					sAggNeuron(end+1) = sAP.sCluster(intClust);
 				end
 				if intNewFile
